624ed67c960b01ea9dfa64ef235b8f15282a91d6
VennyS <panarin.danil.08@gmail.com>
2025-08-08 18:38:06 +0500

    fix: some refactoring

--- apps/frontend/app/services/auth.service.ts
+++ apps/frontend/app/services/auth.service.ts
@@ -1,9 +1,9 @@
-import { axiosPlatform } from "../api/interceptors";
+import { axiosClassic } from "../api/interceptors";
 import { IAuthResponse } from "../types/auth.types";
 
 export const authService = {
   async checkToken() {
-    const response = await axiosPlatform.get<IAuthResponse>("/auth/check");
+    const response = await axiosClassic.get<IAuthResponse>("/auth/check");
     return response.data;
   },
 };
