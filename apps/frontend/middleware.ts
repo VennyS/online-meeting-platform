07d52c219b364751f92402d68c3fa787db682f59
VennyS <panarin.danil.08@gmail.com>
2025-08-08 18:35:53 +0500

    feat: added matcher to middleware

--- apps/frontend/middleware.ts
+++ apps/frontend/middleware.ts
@@ -53,3 +53,7 @@ export async function middleware(req: NextRequest) {
     return res;
   }
 }
+
+export const config = {
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|fonts|public).*)"],
+};
