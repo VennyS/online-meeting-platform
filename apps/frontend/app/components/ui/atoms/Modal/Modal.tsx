1f7a56593d0f1cbff8ecf57af4c934c6a09eb301
VennyS <panarin.danil.08@gmail.com>
2025-10-11 19:15:44 +0500

    feat: added title in modal

--- apps/frontend/app/components/ui/atoms/Modal/Modal.tsx
+++ apps/frontend/app/components/ui/atoms/Modal/Modal.tsx
@@ -1,20 +1,48 @@
-import { Dialog, DialogContent } from "@mui/material";
+import {
+  Box,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+} from "@mui/material";
 import { ModalProps } from "./types";
+import CloseIcon from "@mui/icons-material/Close";
 
-export const Modal = ({ children, onClose }: ModalProps) => {
+export const Modal = ({ children, onClose, title }: ModalProps) => {
   return (
     <Dialog
       open
       onClose={onClose}
       fullWidth
       maxWidth="sm"
+      scroll="body"
       slotProps={{
         paper: {
-          sx: { borderRadius: 2, p: 2 },
+          sx: { borderRadius: 2, p: "20px", bgcolor: "grey.100" },
         },
       }}
     >
-      <DialogContent>{children}</DialogContent>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: !!title ? "space-between" : "flex-end",
+          pb: "8px",
+        }}
+      >
+        {title && (
+          <DialogTitle variant="h5" sx={{ p: 0 }}>
+            {title}
+          </DialogTitle>
+        )}
+        <IconButton
+          title="Закрыть"
+          onClick={onClose}
+          sx={{ color: "#000000b5" }}
+        >
+          <CloseIcon />
+        </IconButton>
+      </Box>
+      <DialogContent sx={{ p: 0 }}>{children}</DialogContent>
     </Dialog>
   );
 };
--- apps/frontend/app/components/ui/atoms/Modal/types.ts
+++ apps/frontend/app/components/ui/atoms/Modal/types.ts
@@ -1,4 +1,5 @@
 export type ModalProps = {
   children: React.ReactNode;
   onClose: () => void;
+  title?: string;
 };
